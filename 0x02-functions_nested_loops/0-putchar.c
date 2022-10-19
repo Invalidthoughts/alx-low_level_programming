5b0cd02f985ad8c33078278ec1085a1c94d25617
Invalidthoughts <raphaeludanoh@gmail.com>
2022-10-19 18:15:07 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * main - prints "_putchar" followed by new line
+ * main - print _putchar
  * is written by invalidthoughts
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("P");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-	return (0);
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('\n');
+return (0);
 }
