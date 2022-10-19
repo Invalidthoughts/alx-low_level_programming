0b41c44cb7b0c25faa3357ced7ae887b47071d09
invalidthoughts <raphaeludanoh@gmail.com>
2022-10-19 09:14:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar" followed by new line
+ * is written by invalidthoughts
+ * Return 0
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("P");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+}
